08c2b8a9a95309a3b594c7784c324a289a03cf65
Brian Hoang <brian.hd.hoang@codurance.com>
2022-02-09 11:48:51 +0000

    refactor

--- src/main/java/codurance/InMemoryProductRepository.java
+++ src/main/java/codurance/InMemoryProductRepository.java
@@ -1,15 +1,17 @@
 package codurance;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class InMemoryProductRepository implements ProductRepository{
+    Map<ProductId, Product> productMap = Map.of(
+        new ProductId(10002), new Product("The Hobbit",5, new ProductId(10002)),
+        new ProductId(20001), new Product("Game of Thrones",9, new ProductId(20001)),
+        new ProductId(10001), new Product("Lord of the Rings",10, new ProductId(10001))
+    );
+
     @Override
     public Product getById(ProductId productId) {
-        if(productId.equals(new ProductId(10002))){
-            return new Product("The Hobbit",5, productId);
-        }
-
-        if(productId.equals(new ProductId(20001))){
-            return new Product("Game of Thrones",9, productId);
-        }
-        return new Product("Lord of the Rings", 10, new ProductId(10001));
+        return productMap.get(productId);
     }
 }
