810e2b87cfa34dcb83d28e16d41a11edde823db2
danihra-codurance <daniel.rodriguez@codurance.com>
2022-02-09 12:43:25 +0100

    red

--- src/main/java/codurance/InMemoryProductRepository.java
+++ src/main/java/codurance/InMemoryProductRepository.java
@@ -3,6 +3,9 @@ package codurance;
 public class InMemoryProductRepository implements ProductRepository{
     @Override
     public Product getById(ProductId productId) {
+        if(productId.equals(new ProductId(10002))){
+            return new Product("The Hobbit",5, productId);
+        }
         return new Product("Lord of the Rings", 10, new ProductId(10001));
     }
 }
--- src/test/java/codurance/ProductRepositoryShould.java
+++ src/test/java/codurance/ProductRepositoryShould.java
@@ -15,7 +15,6 @@ class ProductRepositoryShould {
         Product lord_of_the_rings = new Product("Lord of the Rings", 10, id);
 
         assertEquals(lord_of_the_rings, productRepository.getById(id));
-
     }
 
     @Test
@@ -27,6 +26,17 @@ class ProductRepositoryShould {
         Product the_hobbit = new Product("The Hobbit", 5, id);
 
         assertEquals(the_hobbit, productRepository.getById(id));
+    }
 
+
+    @Test
+    void get() {
+        var productRepository = new InMemoryProductRepository();
+
+        ProductId id = new ProductId(20001);
+
+        Product the_hobbit = new Product("Game of Thrones", 9, id);
+
+        assertEquals(the_hobbit, productRepository.getById(id));
     }
 }
