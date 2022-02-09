ef763b30a773a8d24c6cb5b2ebde0df07c857324
Brian Hoang <brian.hd.hoang@codurance.com>
2022-02-09 11:44:12 +0000

    green

--- src/main/java/codurance/InMemoryProductRepository.java
+++ src/main/java/codurance/InMemoryProductRepository.java
@@ -6,6 +6,10 @@ public class InMemoryProductRepository implements ProductRepository{
         if(productId.equals(new ProductId(10002))){
             return new Product("The Hobbit",5, productId);
         }
+
+        if(productId.equals(new ProductId(20001))){
+            return new Product("Game of Thrones",9, productId);
+        }
         return new Product("Lord of the Rings", 10, new ProductId(10001));
     }
 }
