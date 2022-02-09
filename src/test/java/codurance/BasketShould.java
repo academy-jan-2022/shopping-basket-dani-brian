338c6192ac10d999ea0b23b351dba81e4fead99d
danihra-codurance <daniel.rodriguez@codurance.com>
2022-02-09 14:39:54 +0100

    red

--- src/test/java/codurance/BasketShould.java
+++ src/test/java/codurance/BasketShould.java
@@ -1,10 +1,17 @@
 package codurance;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class BasketShould {
+import java.util.List;
+
+class BasketShould {
     @Test
-    void name() {
+    void return_current_date() {
+        var expected = "2022-02-14";
+        var basket = new Basket(new UserId(), List.of(),expected);
+
+        Assertions.assertEquals(expected,basket.getDate());
 
     }
 }
